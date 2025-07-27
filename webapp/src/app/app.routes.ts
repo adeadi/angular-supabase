497142632d52e11d198fed855ac0f1b410395545
Ade Adi <ade.adiw@gmail.com>
2025-07-27 12:57:06 +0200

    add pricing plan page

--- webapp/src/app/app.routes.ts
+++ webapp/src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
 import { CustomerList } from './customer-list/customer-list';
 import {Login} from './login/login';
-import {Landing} from './landing/landing';
 import {Register} from './register/register';
+import {PricingPlan} from './pricing-plan/pricing-plan';
 
 export const routes: Routes = [
-  { path: '', component: Landing, },
+  { path: '', component: PricingPlan, },
   { path: 'customer', component: CustomerList, },
   { path: 'login', component: Login, },
   { path: 'register', component: Register, },
